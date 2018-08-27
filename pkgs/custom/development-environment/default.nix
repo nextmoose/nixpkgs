6d9fa5d64b5f74444f134e0b10d13822aa791238
Emory Merryman <emory.merryman@gmail.com>
2018-08-27 11:51:51 -0400

    do it

--- pkgs/custom/development-environment/default.nix
+++ pkgs/custom/development-environment/default.nix
@@ -3,12 +3,19 @@ stdenv.mkDerivation {
   name = "development-environment";
   src = fetchgit {
     url = "https://github.com/nextmoose/development-environment.git";
-    rev = "1d5eba74faba3dce85ade6b2d84542e40ed7631b";
+    rev = "30d5db9b1d9db32bfbb0cc8106997744d63b3f7d";
     sha256 = "1i6vbjr3aybr2bd1nc7yghqnv2j9f0iwpq0q5kbx2q2c4vrhy94n";
   };
   installPhase = ''
-    mkdir -p $out/bin &&
+    mkdir $out/bin &&
         cp launch.sh $out/bin/launch-development-environment &&
 	chmod 0555 $out/bin/launch-development-environment
   '';
+  postInstallPhase = ''
+    EXE=$out/bin/launch-development-environment &&
+        mkdir $out/share &&
+    	mkdir $out/share/bash-completion &&
+    	mkdir $out/share/bash-completion/completions
+    	cp completion.sh > $out/share/bash-completion/completions/launch-development-environment
+    '';
 }
